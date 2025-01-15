925bf1786511f167f4f43739aa10dec145238fa3
Michał Bunda <m.bunda.721@studms.ug.edu.pl>
2025-01-15 22:15:56 +0100

    feat(backend): add user class, repo, service, controller

---
+++ backend/src/main/java/com/winglink/backend/domain/User.java
@@ -0,0 +1,34 @@
+package com.winglink.backend.domain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.winglink.backend.enums.UserRole;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @NotBlank(message = "First name is mandatory")
+    private String firstName;
+    @NotBlank(message = "Last name is mandatory")
+    private String lastName;
+    @NotBlank(message = "E-mail is mandatory")
+    @Email(message = "Incorrect e-mail format")
+    private String email;
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Ticket> tickets;
+}
---
+++ backend/src/main/java/com/winglink/backend/service/UserService.java
@@ -0,0 +1,37 @@
+package com.winglink.backend.service;
+
+import com.winglink.backend.domain.User;
+import com.winglink.backend.enums.UserRole;
+import com.winglink.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        user.setRole(UserRole.NORMAL);
+        user.setTickets(Collections.emptyList());
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+}
