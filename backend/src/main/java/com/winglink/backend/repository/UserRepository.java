089f0708b4262eabcd9843713297b0b29a444f8c
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-01-22 16:20:05 +0100

    refactor: change User class name and fix imports

--- backend/src/main/java/com/winglink/backend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.winglink.backend.repository;
-
-import com.winglink.backend.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- backend/src/main/java/com/winglink/backend/service/AppUserService.java
+++ backend/src/main/java/com/winglink/backend/service/AppUserService.java
@@ -1,8 +1,8 @@
 package com.winglink.backend.service;
 
-import com.winglink.backend.entity.User;
+import com.winglink.backend.entity.AppUser;
 import com.winglink.backend.enums.UserRole;
-import com.winglink.backend.repository.UserRepository;
+import com.winglink.backend.repository.AppUserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -10,24 +10,24 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserService {
-    private final UserRepository userRepository;
+public class AppUserService {
+    private final AppUserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    public AppUserService(AppUserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-    public User save(User user) {
+    public AppUser save(AppUser user) {
         user.setRole(UserRole.NORMAL);
         user.setTickets(Collections.emptyList());
         return userRepository.save(user);
     }
 
-    public Optional<User> findById(Long id) {
+    public Optional<AppUser> findById(Long id) {
         return userRepository.findById(id);
     }
 
-    public List<User> findAll() {
+    public List<AppUser> findAll() {
         return userRepository.findAll();
     }
 
