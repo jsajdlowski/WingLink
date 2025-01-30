e9090fa0d3f3f502604f2983896166047ca50c09
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-01-30 17:10:31 +0100

    refactor(backend): add @Service annotation to AirportService

--- backend/src/main/java/com/winglink/backend/service/AirportService.java
+++ backend/src/main/java/com/winglink/backend/service/AirportService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.winglink.backend.entity.Airport;
 import com.winglink.backend.repository.AirportRepository;
 
+@Service
 public class AirportService {
 
     @Autowired
