baef120b7d56acd578b6f3042e69456fcedcda31
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-01-30 16:23:59 +0100

    feat(backend): add AirportService

---
+++ backend/src/main/java/com/winglink/backend/service/AirportService.java
@@ -0,0 +1,31 @@
+package com.winglink.backend.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.winglink.backend.entity.Airport;
+import com.winglink.backend.repository.AirportRepository;
+
+public class AirportService {
+
+    @Autowired
+    private AirportRepository airportRepository;
+
+    public List<Airport> getAllairports() {
+        return airportRepository.findAll();
+    }
+
+    public Optional<Airport> getAirportById(Long id) {
+        return airportRepository.findById(id);
+    }
+
+    public Airport saveAirport(Airport airport) {
+        return airportRepository.save(airport);
+    }
+
+    public void deleteAirport(Long id) {
+        airportRepository.deleteById(id);
+    }
+}
