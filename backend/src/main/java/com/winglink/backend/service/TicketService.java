f101e6df255a9cf1de5aebab34bece5b288df7ae
Michał Bunda <m.bunda.721@studms.ug.edu.pl>
2025-06-16 13:11:38 +0200

    refactor(backend): swap returned tickets to dtos that include flight data

--- backend/src/main/java/com/winglink/backend/service/TicketService.java
+++ backend/src/main/java/com/winglink/backend/service/TicketService.java
@@ -2,6 +2,8 @@ package com.winglink.backend.service;
 
 import com.winglink.backend.config.security.SecurityUtils;
 import com.winglink.backend.dto.TicketDto;
+import com.winglink.backend.dto.TicketDtoConverter;
+import com.winglink.backend.dto.TicketResponseDto;
 import com.winglink.backend.entity.AppUser;
 import com.winglink.backend.entity.Flight;
 import com.winglink.backend.entity.Ticket;
@@ -30,19 +32,19 @@ public class TicketService {
         return ticketRepository.save(ticket);
     }
 
-    public Optional<Ticket> findById(Long id) {
-        return ticketRepository.findById(id);
+    public Optional<TicketResponseDto> findById(Long id) {
+        return ticketRepository.findById(id).map(TicketDtoConverter::convertToTicketDto);
     }
 
-    public List<Ticket> findAll() {
-        return ticketRepository.findAll();
+    public List<TicketResponseDto> findAll() {
+        return ticketRepository.findAll().stream().map(TicketDtoConverter::convertToTicketDto).toList();
     }
 
     public void deleteById(Long id) {
         ticketRepository.deleteById(id);
     }
 
-    public Ticket buyTicket(TicketDto ticketDto) {
+    public TicketResponseDto buyTicket(TicketDto ticketDto) {
         String auth0Id = SecurityUtils.getAuth0UserId();
         AppUser user = appUserService.getUserByAuth0Id(auth0Id).orElseThrow(Auth0UserNotFoundInDbException::new);
         Flight flight = flightService.findFlightById(ticketDto.flightId()).orElseThrow(FlightNotFoundException::new);
@@ -50,7 +52,7 @@ public class TicketService {
         String firstName = ticketDto.firstName() == null ? user.getFirstName() : ticketDto.firstName();
         String lastName = ticketDto.lastName() == null ? user.getLastName() : ticketDto.lastName();
         Ticket ticket = new Ticket(null, firstName, lastName, ticketDto.seatClass(), flight, user);
-        return ticketRepository.save(ticket);
+        return TicketDtoConverter.convertToTicketDto(ticketRepository.save(ticket));
     }
 
     private boolean isUnderSeatLimit(Flight flight) {
