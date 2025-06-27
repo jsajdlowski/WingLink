e2dbb18072611d2e086ef55527fb7e6467fc23b6
Julia Sajdlowski <juliasajdlowski@gmail.com>
2025-06-27 10:00:15 +0200

    feat(frontend): replace SVG with airplane image in NotFound and update titles in localization files

--- frontend/src/components/not-found/not-found.tsx
+++ frontend/src/components/not-found/not-found.tsx
@@ -2,6 +2,7 @@ import { useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import { Center, Title, Text, Button, Stack, Box } from '@mantine/core'
 import { IconArrowLeft } from '@tabler/icons-react'
+import airplaneSvg from '../../assets/plane.svg'
 
 export const NotFound = () => {
   const { t } = useTranslation('notFound')
@@ -33,23 +34,14 @@ export const NotFound = () => {
               filter: 'drop-shadow(0px 4px 6px rgba(0, 0, 0, 0.1))',
             }}
           >
-            <svg
-              width="100%"
-              height="100%"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                <path
-                  d="M22,16 L22,14.5 C22,14.2238576 21.7761424,14 21.5,14 L16.7002966,14 L9.30029663,9 L3.5,9 C3.22385763,9 3,9.22385763 3,9.5 L3,11 L9.30029663,11 L13.5526705,14 L8.30029663,14 L6.30029663,13 L3.5,13 C3.22385763,13 3,13.2238576 3,13.5 L3,15 L7.30029663,15 L9.30029663,16 L21.5,16 C21.7761424,16 22,15.7761424 22,15.5 L22,16 Z"
-                  fill="#4DABF7"
-                />
-                <path
-                  d="M22,15.5 L22,14.5 C22,14.2238576 21.7761424,14 21.5,14 L16.7002966,14 L9.30029663,9 L3.5,9 C3.22385763,9 3,9.22385763 3,9.5 L3,10.5 C3,10.2238576 3.22385763,10 3.5,10 L9.30029663,10 L16.7002966,15 L21.5,15 C21.7761424,15 22,14.7761424 22,14.5 L22,15.5 Z M22,15.5 L22,14.5 C22,14.2238576 21.7761424,14 21.5,14 L16.7002966,14 L13.5526705,14 L9.30029663,13 L3.5,13 C3.22385763,13 3,13.2238576 3,13.5 L3,14.5 C3,14.2238576 3.22385763,14 3.5,14 L7.30029663,14 L9.30029663,15 L21.5,15 C21.7761424,15 22,14.7761424 22,14.5 L22,15.5 Z"
-                  fill="#228BE6"
-                />
-              </g>
-            </svg>
+            <img
+              src={airplaneSvg}
+              alt="Airplane"
+              style={{
+                width: '100%',
+                height: '100%',
+              }}
+            />
           </Box>
           <style>
             {`
@@ -65,13 +57,21 @@ export const NotFound = () => {
           </style>
         </Box>
 
-        <Title order={1} style={{ fontSize: '3rem', color: '#1c7ed6' }}>
+        <Title
+          order={1}
+          style={{ fontSize: '3rem', color: '#1c7ed6', textAlign: 'center' }}
+        >
           {t('title')}
         </Title>
 
         <Title
           order={2}
-          style={{ fontSize: '1.5rem', color: '#4dabf7', marginTop: '-15px' }}
+          style={{
+            fontSize: '1.5rem',
+            color: '#4dabf7',
+            marginTop: '-15px',
+            textAlign: 'center',
+          }}
         >
           {t('message')}
         </Title>
